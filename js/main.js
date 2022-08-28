4061011490ae1afc280a1c05a861ea44c5108588
matchmydesign <112019419+matchmydesign@users.noreply.github.com>
2022-08-29 02:01:28 +0530

    v2

---
+++ js/main.js
@@ -0,0 +1,67 @@
+(function ($) {
+    "use strict";
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbyHXgXItrdgz4i1AhCFdgTIog8qY_c-UYB5RVYqKTaTIayek8PvsdpcLAGU3OLP9G1Olw/exec';
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+
+
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+        } else {
+            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+        }
+    });
+
+
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        items: 1,
+        dots: false,
+        loop: true,
+        nav: true,
+        navText: [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
+    const form = document.forms['appointment'];
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+        debugger
+        console.log(form);
+        fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+            .then(response => console.log('Success!', response))
+            .catch(error => console.error('Error!', error.message))
+    });
+
+})(jQuery);
+
